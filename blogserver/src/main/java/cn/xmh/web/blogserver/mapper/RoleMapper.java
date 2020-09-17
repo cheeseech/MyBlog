6b2653d1089237a2b94ad7af069dc2ed592b3fbc
徐茂华 <1355125277@qq.com>
2020-09-17 20:58:47 +0800

    使用beforeRouteEnter优化路由组件切换

---
+++ blogserver/src/main/java/cn/xmh/web/blogserver/mapper/RoleMapper.java
@@ -0,0 +1,8 @@
+package cn.xmh.web.blogserver.mapper;
+
+/**
+ * @author Xmh
+ * @date 2020/9/9 10:49
+ */
+public interface RoleMapper {
+}
---
+++ blogserver/src/main/java/cn/xmh/web/blogserver/model/Role.java
@@ -0,0 +1,8 @@
+package cn.xmh.web.blogserver.model;
+
+/**
+ * @author Xmh
+ * @date 2020/9/9 9:42
+ */
+public class Role {
+}
