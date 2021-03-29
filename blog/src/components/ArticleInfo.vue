48add20868eb73529cabf003a05449d2e711a0dc
徐茂华 <1355125277@qq.com>
2021-03-29 16:09:40 +0800

    完善文章信息组件功能。

--- blog/src/components/ArticleInfo.vue
+++ blog/src/components/ArticleInfo.vue
@@ -1,3 +1,11 @@
+<!--
+ * @FileDescription: 文章列表组件
+ * @Author: 徐茂华
+ * @Date: 2020-08-09 17:55:42
+ * @LastEditors: 徐茂华
+ * @LastEditTime: 2021-02-12 14:58:53
+ * @FilePath: \src\components\ArticleInfo.vue
+-->
 <template>
   <div id="BlogArticle">
     <el-card
@@ -6,7 +14,6 @@
       :key="article.articleId"
       shadow="hover"
     >
-    
       <!--文章主要内容-->
       <div style="padding: 20px">
         <!--是否推荐-->
@@ -20,14 +27,9 @@
         </svg>
 
         <!--原创、转载、翻译-->
-        <el-button
-          type="warning"
-          round
-          style="float: right"
-          class="btnPadded"
-          @click="getArticlesByType(article.typeName)"
-          >{{ article.typeName }}</el-button
-        >
+        <el-button type="warning" round class="btnPadded type-button">{{
+          article.typeName
+        }}</el-button>
         <!--专栏-->
         <el-button
           @click="goCategory(article.cateName)"
@@ -38,10 +40,13 @@
           >{{ article.cateName }}</el-button
         >
         <!--标题-->
-        <h2 class="m-article">{{ article.title }}</h2>
+        <h2 class="m-article-title" @click="goArticleInfo(article.articleId)">
+          <a href="javascript:void(0)">{{ article.title }}</a>
+        </h2>
+
         <!--日期-->
         <p style="color: gray">
-          {{ article.publishTime | monthDayYearFormat }}
+          {{ article.updateTime | monthDayYearFormat }}
         </p>
         <!--文章内容-->
         <p class="m-text-lined">{{ article.summary }}</p>
@@ -73,15 +78,6 @@
             <span class="m-article-icon-text">{{ article.comments }}</span>
           </el-menu-item>
 
-          <!--点赞-->
-          <el-menu-item class="m-lf-padded-mini" style="float: right">
-            <svg class="icon m-svg-size m-article-icon" aria-hidden="true">
-              <use xlink:href="#icon-icon4"></use>
-            </svg>
-
-            <span class="m-article-icon-text">{{ article.likes }}</span>
-          </el-menu-item>
-
           <!--浏览次数-->
           <el-menu-item class="m-lf-padded-mini" style="float: right">
             <svg class="icon m-svg-size m-article-icon" aria-hidden="true">
@@ -97,20 +93,12 @@
 </template>
 
 <script>
-import { getRequest } from "../../untils/axiosApi";
-import { postRequest } from "../../untils/axiosApi";
 import { eventBus } from "@/main";
+import { getRequest, postRequest } from "../../untils/axiosApi";
 
 export default {
   name: "BlogArticle",
   props: ["initial_articles"],
-  data() {
-    return {
-      imgSrc: require("./../assets/images/content.jpg"),
-      categoryName: null,
-      typeName: null
-    };
-  },
   created() {
     //兄弟组件传值：根据搜索框值检索文章
     eventBus.$on("title", data => {
@@ -118,40 +106,57 @@ export default {
     });
   },
   methods: {
-    //根据文章类别检索文章
-    getArticlesByType(value) {
-      getRequest("/article/types/" + value).then(response => {
-        if (response.status == 200) {
-          this.articles = response.data;
-        }
-      });
-    },
-    //根据文章标题模糊检索文章
+    /**
+     * @description: 根据文章标题模糊检索文章
+     * @param {String} value
+     * @return {void}
+     */
     getArticlesByTitle(value) {
-      getRequest("/article/" + value).then(response => {
+      postRequest("/article/" + value).then(response => {
         if (response.status == 200) {
           this.articles = response.data;
         }
       });
     },
-    //传递标签名并跳转到标签页
+
+    /**
+     * @description: 传递标签名并跳转到标签页
+     * @param {String} value
+     * @return {void}
+     */
     goTags(value) {
       this.$router.push({
         name: "标签",
         params: { value }
       });
     },
-    //传递专栏名并跳转到专栏页
+
+    /**
+     * @description: 传递专栏名并跳转到专栏页
+     * @param {String} value
+     * @return {void}
+     */
     goCategory(value) {
       this.$router.push({
         name: "专栏",
         params: { value }
       });
+    },
+
+    /**
+     * @description: 传递文章ID并跳转到文章详情页
+     * @param {Number} value
+     * @return {void}
+     */
+    goArticleInfo(value) {
+      this.$router.push({
+        name: "文章详情",
+        params: { articleId: value }
+      });
     }
   }
 };
 </script>
-
 <style scoped>
 .m-lf-padded-mini {
   padding-left: 0.4em;
@@ -163,9 +168,28 @@ export default {
 }
 
 /*文章标题*/
-.m-article {
+.m-article-title {
   margin-top: 0;
   margin-bottom: 0;
   display: inline-block;
+}
+
+.m-article-title a {
+  color: #000;
+}
+
+.m-article-title a:hover {
+  color: #3476d2;
+}
+
+.type-button {
+  cursor: default;
+  float: right;
+}
+.type-button:focus,
+.type-button:hover {
+  color: #fff;
+  background-color: #e6a23c;
+  border-color: #e6a23c;
 }
 </style>
